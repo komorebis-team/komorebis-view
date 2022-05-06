ee2420560947f175e2a6cf4b82ef1a16f31dd0f6
diego-urgell <digourgell@outlook.com>
2022-05-06 16:50:34 -0500

    Add the dummy auth

--- src/components/auth.js
+++ src/components/auth.js
@@ -11,34 +11,60 @@ const auth = {
         triedLogin: false,
     },
     signin: (username, password, callback) => {
-        // Dummy login. This should rather call our auth API.
-        axios.post(
-            miAuthURL+"signin",
-            {
-                username: username,
-                password: password
-            }
-        ).then( function (response){
-            console.log(response);
-            axios.get(
-                miAuthURL+"profile",
+    console.log("In Dummy login")
+    if (username === "admin" && password === "admin") {
+        auth.user.name = "Diego"
+        auth.user.lastname = "Urgell"
+        auth.user.username = "dieji@hotmail.com"
+        auth.user.role = "admin"
+        auth.user.triedLogin = false;
+    } else if (username === "agent" && password === "agent"){
+        auth.user.name = "Agustín"
+        auth.user.lastname = "Abreu"
+        auth.user.username = "agus@hotmail.com"
+        auth.user.role = "agent"
+        auth.user.triedLogin = false;
+    } else{
+            auth.user.triedLogin = true;
+            auth.user.name = ""
+            auth.user.lastname = ""
+            auth.user.username = ""
+            auth.user.role = ""
+    }
+        /* This is used for login using axios. It is commented since we still dont have a remote server.
+        else {
+
+            axios.post(
+                miAuthURL + "signin",
                 {
-                    headers: { Authorization: `Bearer ${response.data.token}` }
+                    username: username,
+                    password: password
                 }
-            ).then( function(response2){
-                console.log(response2);
-                auth.user.triedLogin = false;
-                auth.user.name = response2.data.name;
-                auth.user.lastname = "";
-                auth.user.username = response2.data.name;
-                console.log(auth.user);
-                auth.user.role = response2.data.principal.role.toLowerCase();
-                console.log(auth.user);
-                localStorage.setItem("user", JSON.stringify(auth.user));
-                callback();
-                }
-            )
-        });
+            ).then(function (response) {
+                console.log(response);
+                axios.get(
+                    miAuthURL + "profile",
+                    {
+                        headers: {Authorization: `Bearer ${response.data.token}`}
+                    }
+                ).then(function (response2) {
+                        console.log(response2);
+                        auth.user.triedLogin = false;
+                        auth.user.name = response2.data.name;
+                        auth.user.lastname = "";
+                        auth.user.username = response2.data.name;
+                        console.log(auth.user);
+                        auth.user.role = response2.data.principal.role.toLowerCase();
+                        console.log(auth.user);
+                        localStorage.setItem("user", JSON.stringify(auth.user));
+                        callback();
+                    }
+                )
+            });
+        }
+        */
+        localStorage.setItem("user", JSON.stringify(auth.user));
+        callback();
     },
     signout: (callback) => {
         auth.user.triedLogin = false;
